56dd4c948a531ff93234e905a945cd82d7120fed
Ninad Sonawane <77869600+ninadsonawane@users.noreply.github.com>
2023-01-10 20:48:47 +0530

    test

--- client/components/Header.js
+++ client/components/Header.js
@@ -4,7 +4,7 @@ import { ConnectButton } from 'web3uikit';
 function Header() {
   return (
     <nav className="p-5 border-b-2 flex flex-row">
-      <h1 className="py-4 px-4 font-bold text-3xl">Staking App</h1>
+      <h1 className="py-4 px-4 font-bold text-3xl">StakingApp</h1>
       <div className="ml-auto py-2 px-4">
         <ConnectButton moralisAuth={false} />
       </div>
